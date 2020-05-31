64eab1681d2f5864df5872cd0d5cdfd16925f742
LABBASSIBassem <49342568+LABBASSIBassem@users.noreply.github.com>
2020-05-31 22:05:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ProductTable from './components/ProductTable';
+
+
+ const products = [
+    {
+      category : 'Electronics',
+      name : 'Laptop',
+      price : 1500,
+      id : 1,
+    },
+  
+    {
+      category : 'Clothes',
+      name : 'Shirt',
+      price : 15 ,
+      id : 2,
+    },
+    {
+      category : 'Electronics',
+      name : 'Desktop',
+      price : 1800,
+      id : 3,
+    },
+  
+    {
+      category : 'Clothes',
+      name : 'Skirt',
+      price : 20 ,
+      id : 4,
+    },
+    {
+      category : 'Electronics',
+      name : 'Mouse',
+      price : 25,
+      id : 5,
+    },
+  
+    {
+      category : 'Clothes',
+      name : 'Hoddie',
+      price : 40 ,
+      id : 6,
+    },
+  ];
 
 
 ReactDOM.render(
    
   <React.StrictMode>
-    <App/>
+     <ProductTable products={products}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
